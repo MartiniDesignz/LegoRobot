8a081078bfebffebe95c246cbdb68623721f63ea
MartiniDesignz <martinidesignz@gmail.com>
2019-03-23 22:00:10 -0400

    Create playSound.py

---
+++ playSound.py
@@ -0,0 +1,8 @@
+#Initializing
+from ev3dev2.sound import Sound
+snd=Sound()
+
+
+# snd.get_volume()
+# snd.set_volume(x)
+# snd.play_file(("/home/robot/Sound/file.wav"), volume=100, play_type="PLAY_NO_WAIT_FOR_COMPLETE")
